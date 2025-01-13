312588ca812a509a703dcf1864d3c073bdc13dab
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-01-13 22:07:19 +0700

    Modul1

---
+++ app/src/main/java/org/d3if0023/mymodul1/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.d3if0023.mymodul1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.d3if0023.mymodul1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvHello.text = "hello binding"
+
+    }
+}
