87922b17815d720c8b63da88c13c09c4e24999a7
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-01-20 19:09:21 +0700

    3.3.Mendeteksi Wajah dari Gambar Kamera

--- app/src/main/java/org/d3if0023/mymodul1/MainActivity.kt
+++ app/src/main/java/org/d3if0023/mymodul1/MainActivity.kt
@@ -3,15 +3,25 @@ package org.d3if0023.mymodul1
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.Analyzer
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import org.d3if0023.mymodul1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +36,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraProvider: ProcessCameraProvider
     private lateinit var preview: Preview
+    private lateinit var imageAnalyzer: ImageAnalysis
+    private lateinit var detector: FaceDetector
+    private var lastImage: ImageProxy? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,12 +78,21 @@ class MainActivity : AppCompatActivity() {
                 PackageManager.PERMISSION_GRANTED
     }
     private fun startCamera() {
+        val option = FaceDetectorOptions.Builder()
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+        detector = FaceDetection.getClient(option)
+
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             cameraProvider = cameraProviderFuture.get()
 
             preview = Preview.Builder().build()
             preview.setSurfaceProvider(binding.imageView.surfaceProvider)
+
+            val executor = ContextCompat.getMainExecutor(this)
+            imageAnalyzer = ImageAnalysis.Builder().build()
+            imageAnalyzer.setAnalyzer(executor) { analyze(it) }
 
             cameraLive()
         }, ContextCompat.getMainExecutor(this))
@@ -79,9 +101,29 @@ class MainActivity : AppCompatActivity() {
         val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
         try{
             cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview,
+                imageAnalyzer)
         } catch(e: Exception) {
             Log.e("MainActivity", "Error kamera: "+e.message)
         }
+    }
+
+    @OptIn(ExperimentalGetImage::class)
+    private fun analyze(imageProxy: ImageProxy){
+        lastImage = imageProxy
+        val mediaImage = imageProxy.image ?: return
+
+        val rotation = imageProxy.imageInfo.rotationDegrees
+        val image = InputImage.fromMediaImage(mediaImage, rotation)
+
+        detector.process(image)
+            .addOnSuccessListener {
+                Log.d("WAJAH","Wajah terdeteksi: " + it.size.toString())
+                lastImage?.close()
+            }
+            .addOnFailureListener { e ->
+                Log.e("MainActivity", "error deteksi wajah: " +
+                        e.message)
+            }
     }
 }
