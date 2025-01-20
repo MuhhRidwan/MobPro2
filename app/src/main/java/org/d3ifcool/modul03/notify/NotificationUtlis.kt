61201e717654794c91550659f9c20be61e555571
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-01-20 10:53:21 +0700

    3.3.Membuat agar notifikasi dapat diklik

--- app/src/main/java/org/d3ifcool/modul03/notify/NotificationUtlis.kt
+++ app/src/main/java/org/d3ifcool/modul03/notify/NotificationUtlis.kt
@@ -2,20 +2,29 @@ package org.d3ifcool.modul03.notify
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import org.d3if0023.mymodul1.MainActivity
 import org.d3if0023.mymodul1.R
 
 private const val NOTIFICATION_ID = 0
 fun NotificationManager.sendNotification(context: Context) {
+    val intent = Intent(context, MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context,
+        NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
     val builder = NotificationCompat.Builder(context,
         context.getString(R.string.notif_channel_id)
     )
         .setSmallIcon(R.mipmap.ic_launcher_round)
         .setContentTitle(context.getString(R.string.notif_title))
         .setContentText(context.getString(R.string.notif_message))
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
     createChannel(context)
     notify(NOTIFICATION_ID, builder.build())
 }
