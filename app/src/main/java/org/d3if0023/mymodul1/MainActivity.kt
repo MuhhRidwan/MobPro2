23c033f7c661cdf91d6b3232b8ee53421c90324a
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-01-20 17:08:02 +0700

    3.1.Meminta Izin Kamera ke Pengguna

--- app/src/main/java/org/d3if0023/mymodul1/MainActivity.kt
+++ app/src/main/java/org/d3if0023/mymodul1/MainActivity.kt
@@ -1,22 +1,64 @@
 package org.d3if0023.mymodul1
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.d3if0023.mymodul1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val REQUEST_CODE_PERMISSIONS = 1
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            android.Manifest.permission.CAMERA)
+    }
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.tvHello.text = "HelloBinding!"
+        if (allPermissionsGranted()) {
+            startCamera()
+        }
+        else {
+            requestPermission()
+            binding.mulaiButton.setOnClickListener{ requestPermission() }
+        }
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+        permissions: Array<String>, results: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, results)
+
+        if (requestCode== REQUEST_CODE_PERMISSIONS && allPermissionsGranted()) {
+            startCamera()
+            binding.mulaiButton.visibility = View.GONE
+        }else{
+            Toast.makeText(this,
+                R.string.tidak_dapat_izin,Toast.LENGTH_LONG).show()
+            binding.mulaiButton.visibility = View.VISIBLE
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+            REQUEST_CODE_PERMISSIONS)
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+    private fun startCamera() {
+        Log.d("KAMERA", "Izin kamera telah diberikan.")
     }
 }
