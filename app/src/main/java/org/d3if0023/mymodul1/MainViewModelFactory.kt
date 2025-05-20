3ead5cbf48123c68ee82d13c3c7bf403eb1a8fde
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-05-21 06:44:42 +0700

    last commit

---
+++ app/src/main/java/org/d3if0023/mymodul1/MainViewModelFactory.kt
@@ -0,0 +1,16 @@
+package org.d3if0023.mymodul1
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.d3if0023.mymodul1.data.MahasiswaDao
+
+class MainViewModelFactory(private val dataSource: MahasiswaDao) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(dataSource) as T
+        }
+        throw IllegalArgumentException("Unable to construct ViewModel")
+    }
+}
---
+++ app/src/main/java/org/d3if0023/mymodul1/data/MahasiswaDao.kt
@@ -0,0 +1,18 @@
+package org.d3if0023.mymodul1.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MahasiswaDao {
+    @Insert
+    fun insertData(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nim")
+    fun getData(): LiveData<List<Mahasiswa>>
+
+    @Query("DELETE FROM mahasiswa WHERE id IN (:ids)")
+    fun deleteData(ids: List<Int>)
+}
