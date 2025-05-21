17d65d14107c9474db4d810d6e875fd85e03cca4
Muhammad Ridwan <mridwanrachmat07@gmail.com>
2025-05-21 07:02:08 +0700

    3.4 belum

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
 }
